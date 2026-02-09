1f90916c8db088561a9fcccc006c76703e858eb3
Fiantso Harena <fiantsorav@gmail.com>
2026-02-09 23:31:47 +0300

    chore: add ServiceResponse class and related utilities

--- apps/api/src/common/utils/env-config.ts
+++ apps/api/src/common/utils/env-config.ts
@@ -8,6 +8,12 @@ const envSchema = z.object({
   HOST: z.string().nonempty().default('localhost'),
   PORT: z.coerce.number().int().positive().default(8080),
   CORS_ORIGIN: z.string().default('http://localhost:3000'),
+
+  DATABASE_URL: z.string().url(),
+
+  FIREBASE_PRIVATE_KEY: z.string().nonempty(),
+  FIREBASE_CLIENT_EMAIL: z.string().nonempty(),
+  FIREBASE_PROJECT_ID: z.string().nonempty(),
 });
 
 const parsedEnv = envSchema.safeParse(process.env);
