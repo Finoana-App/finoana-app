6e5ea204d3a23511bd16aad676ad9bc65c74ca85
Fiantso Harena <fiantsorav@gmail.com>
2026-02-26 16:26:02 +0300

    feat(not-implemented): add unauthorized page component

---
+++ apps/web/app/[lang]/dashboard/unauthorized/page.tsx
@@ -0,0 +1,7 @@
+export default function UnauthorizedPage() {
+  return (
+    <section className="flex h-screen w-full items-center justify-center">
+      <h1>Unauthorized</h1>
+    </section>
+  );
+}
